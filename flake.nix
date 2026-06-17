6e287be5123560cf249f6fda4b9e86519f55d099
kaeeraa <kaeeraa@nebula-nook.ru>
2026-06-17 20:07:59 +0300

    fix: package nix build with submodule source

--- flake.nix
+++ flake.nix
@@ -15,8 +15,23 @@
       system: let
         pkgs = import nixpkgs {inherit system;};
         naersk-lib = pkgs.callPackage naersk {};
+        containerdSrc = builtins.fetchGit {
+          url = "https://github.com/stratumix/rust-extensions";
+          rev = "dbb1eb93578c9e1fffe06e8e482f94e6f1bfb757";
+          submodules = true;
+        };
+        src = pkgs.runCommand "orvyn-src" {} ''
+          mkdir -p "$out"
+          cp -R ${./.}/. "$out/"
+          rm -rf "$out/vendor/containerd-client"
+          mkdir -p "$out/vendor/containerd-client"
+          cp -R ${containerdSrc}/. "$out/vendor/containerd-client/"
+        '';
       in {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = naersk-lib.buildPackage {
+          inherit src;
+          nativeBuildInputs = [pkgs.protobuf];
+        };
         devShell = with pkgs;
           mkShell {
             buildInputs = [protobuf cargo rustc rustfmt pre-commit rustPackages.clippy];
