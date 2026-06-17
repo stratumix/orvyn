b1ebdbc8bca4bf5f9e113cec9fba4725e9b647a3
kaeeraa <kaeeraa@nebula-nook.ru>
2026-06-17 20:07:49 +0300

    refactor: simplify container client singleton

--- src/instances.rs
+++ src/instances.rs
@@ -5,29 +5,20 @@ use containerd_client::{
 };
 use tokio::sync::OnceCell;
 
-static CHANNEL: OnceCell<Channel> = OnceCell::const_new();
 static CONTAINERS_CLIENT: OnceCell<ContainersClient<Channel>> = OnceCell::const_new();
 
-async fn get_channel() -> Channel {
-    match CHANNEL
-        .get_or_try_init(|| async { connect("/run/containerd/containerd.sock").await })
-        .await
-    {
-        Ok(channel) => channel.clone(),
-        Err(err) => {
-            eprintln!("failed to connect to channel: {err:?}");
-            exit(1);
-        }
-    }
-}
-
 pub async fn get_containers_client() -> ContainersClient<Channel> {
-    let client = CONTAINERS_CLIENT
+    CONTAINERS_CLIENT
         .get_or_init(|| async {
-            let channel = get_channel().await;
+            let channel = connect("/run/containerd/containerd.sock")
+                .await
+                .unwrap_or_else(|err| {
+                    eprintln!("failed to connect to channel: {err:?}");
+                    exit(1);
+                });
+
             ContainersClient::new(channel)
         })
-        .await;
-
-    client.clone()
+        .await
+        .clone()
 }
